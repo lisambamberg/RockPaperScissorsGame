ca7013b2c4f90a6646d997f2eb86e2726dbea629
Lisa Bamberg <lisambamberg@gmail.com>
2019-01-30 14:22:49 -0600

    Added Computer Choice

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-console.log("hi!")
-console.log("rockpaperscissors")
+const computerPlay = () => {
+    const computerInput = Math.floor(Math.random() * Math.floor(3));
+    if (computerInput === 0) {
+        return 'rock';
+    }
+    if (computerInput === 1) {
+        return 'paper';
+    }
+    if (computerInput === 2) {
+        return 'scissors';
+    }
+}
+
+
+console.log(computerPlay());
