522623020751f5c3d12d8b3241cd5dd8beaa45b8
Lisa Bamberg <lisambamberg@gmail.com>
2019-01-30 15:01:41 -0600

    Wrote code for playGame function that determines winner

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 const computerPlay = () => {
     const computerInput = Math.floor(Math.random() * Math.floor(3));
-    if (computerInput === 0) {
+    if (computerInput === 0){
         return 'rock';
     }
-    if (computerInput === 1) {
+    if (computerInput === 1){
         return 'paper';
     }
-    if (computerInput === 2) {
+    if (computerInput === 2){
         return 'scissors';
     }
 }
 
+//const playerOne = 'scissors';
+//const playerTwo = 'paper';
 
-console.log(computerPlay());
+const humanSelection = 'rock';
+const computerSelection = computerPlay();
+
+const playGame = (human, computer) => {
+    if (human === 'rock' && computer === 'scissors' || 
+    human === 'scissors' && computer === 'paper' ||
+    human === 'paper' && computer === 'rock'){
+        return 'You win!';
+    }
+    if (human === 'rock' && computer === 'paper' ||
+    human === 'scissors' && computer === 'rock' ||
+    human === 'paper' && computer === 'scissors'){
+        return 'Computer wins!';
+    }
+
+    if (human === computer){
+        return 'It\'s a draw';
+    }
+}
+console.log('Human selection ', humanSelection);
+console.log('Computer selection ', computerSelection);
+console.log(playGame(humanSelection, computerSelection));
